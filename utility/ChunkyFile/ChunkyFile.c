745705546bf17bcb40fc429a3032e551cf2d8530
Jacob Weiss <jacob@kidzilla.com>
2016-05-18 16:20:16 -0400

    Fixed bug in filesystem. Added chunky file.

---
+++ utility/ChunkyFile/ChunkyFile.c
@@ -0,0 +1,33 @@
+#include "ChunkyFile.h"
+
+
+typedef struct ChunkyFile
+{
+	char* path;
+	char* data;
+	int size;
+} _ChunkyFile;
+
+ChunkyFile* chunkyfile_new_from_path(char* path)
+{
+}
+
+ChunkyFile* chunkyfile_new_empty(int size)
+{
+}
+
+void chunkyfile_write_to_path(ChunkyFile* chunkyfile, char* path)
+{
+}
+
+int chunkyfile_num_chunks(ChunkyFile* chunkyfile)
+{
+}
+
+void chunkyfile_get_chunk(ChunkyFile* chunkyfile, int chunkNum, char** chunk, int* chunkSize)
+{
+}
+
+void chunkyfile_set_chunk(ChunkyFile* chunkyfile, int chunkNum, char*  chunk, int  chunkSize)
+{
+}
